3868bdfc19a35f12aec84860216484fd9cf8def3
Tanat <txb9038@g.rit.edu>
2016-02-24 16:56:50 -0500

    Completed basic requirements for the GameObject class

--- blank-canvas/blank-canvas/GameObject.cs
+++ blank-canvas/blank-canvas/GameObject.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace blank_canvas
 {
@@ -10,20 +13,140 @@ namespace blank_canvas
     /// </summary>
     class GameObject
     {
-        //needs to be done
+
         #region variables
+        Texture2D texture;
+        Vector2 position;
+        Rectangle rectangle;
+        Color color;
         #endregion
 
-        //needs to be done
         #region constructor
+        /// <param name="rectangle">Target Rectangle</param>
+        public GameObject(Rectangle rectangle)
+        {
+            this.rectangle = rectangle;
+            position = new Vector2(rectangle.X, rectangle.Y);
+            color = Color.White;
+        }
+
+        /// <param name="rectangle">Target Rectangle</param>
+        /// <param name="color">Color of the object</param>
+        public GameObject(Rectangle rectangle, Color color)
+        {
+            this.rectangle = rectangle;
+            position = new Vector2(rectangle.X, rectangle.Y);
+            this.color = color;
+        }
+
+        /// <param name="texture">Texture of the object</param>
+        /// <param name="position">Position of the object</param>
+        public GameObject(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+            rectangle = new Rectangle((int)position.X,
+                (int)position.Y, texture.Width, texture.Height);
+            color = Color.White;
+        }
+
+        /// <param name="texture">Texture of the object</param>
+        /// <param name="position">Position of the object</param>
+        /// <param name="color">Color of object</param>
+        public GameObject(Texture2D texture, Vector2 position, Color color)
+        {
+            this.texture = texture;
+            this.position = position;
+            rectangle = new Rectangle((int)position.X,
+                (int)position.Y, texture.Width, texture.Height);
+            this.color = color;
+        }
+
+        /// <param name="texture">Texture of the object</param>
+        /// <param name="rectangle">Target rectangle</param>
+        public GameObject(Texture2D texture, Rectangle rectangle)
+        {
+            this.texture = texture;
+            position = new Vector2((float)rectangle.X, (float)rectangle.Y);
+            this.rectangle = rectangle;
+            color = Color.White;
+        }
+
+        /// <param name="texture">Texture of the object</param>
+        /// <param name="rectangle">Target rectangle</param>
+        /// <param name="color">Color of object</param>
+        public GameObject(Texture2D texture, Rectangle rectangle, Color color)
+        {
+            this.texture = texture;
+            position = new Vector2((float)rectangle.X, (float)rectangle.Y);
+            this.rectangle = rectangle;
+            this.color = color;
+        }
+
         #endregion
 
-        //needs to be done
         #region properties
+        /// <summary>
+        /// The X position of upper left corner
+        /// </summary>
+        public float X
+        {
+            get { return position.X; }
+            set { position.X = value; }
+        }
+        
+        /// <summary>
+        /// The Y position of the upper left corner
+        /// </summary>
+        public float Y
+        {
+            get { return position.Y; }
+            set { position.Y = value; }
+        }
+
+        /// <summary>
+        /// The rectangle
+        /// </summary>
+        public Rectangle Rectangle
+        {
+            get { return rectangle; }
+            set { rectangle = value; }
+        }
+
+        /// <summary>
+        /// The current texture
+        /// </summary>
+        public Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+
+        /// <summary>
+        /// The color of the object
+        /// </summary>
+        public Color Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
         #endregion
 
-        //needs to be done
         #region methods
+        /// <summary>
+        /// Basic draw method, doesn't draw if texture is not instantiated. 
+        /// Default color is white.
+        /// </summary>
+        /// <param name="spriteBatch">The current spriteBatch</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (texture != null)
+            {
+                if (color != null)
+                    spriteBatch.Draw(texture, rectangle, color);
+                else spriteBatch.Draw(texture, rectangle, Color.White);
+            }
+        }
         #endregion
     }
 }
