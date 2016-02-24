573b1dc22103bdde8dc9f029cc1df3fa0b5cca22
Tanat <txb9038@g.rit.edu>
2016-02-24 15:58:23 -0500

    Setting up basic classes

---
+++ blank-canvas/blank-canvas/GameObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace blank_canvas
+{
+    /// <summary>
+    /// <para>The parent class of all objects in the game</para>
+    /// </summary>
+    class GameObject
+    {
+        //needs to be done
+        #region variables
+        #endregion
+
+        //needs to be done
+        #region constructor
+        #endregion
+
+        //needs to be done
+        #region properties
+        #endregion
+
+        //needs to be done
+        #region methods
+        #endregion
+    }
+}
