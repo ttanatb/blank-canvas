3a6f2e270f73c4dbb862610657fffdcacbd2358c
Samantha Heckle (RIT Student) <sxh4716@ad.rit.edu>
2016-03-04 16:19:39 -0500

    Updated StageReader.cs

---
+++ blank-canvas/blank-canvas/StageReader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+/*
+    Class: StageReader
+    Purpose: reads in the stage from a text file
+
+*/
+
+namespace blank_canvas
+{
+    class StageReader
+    {
+        // sets filename
+        private string[] filename;
+        // the path for the original file
+        private string sourcePath;
+
+        // constructor that gets string
+        public StageReader()
+        {
+            try
+            {
+                sourcePath = Path.GetFullPath(@"stage-builder\bin\Debug");
+                filename = Directory.GetFiles(sourcePath, "*.txt");
+                //Console.WriteLine(sourcePath + "\n" + filename[0]);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Error. Press any key.");
+            }
+
+        }
+
+        // moves and reads in file
+        public void ReadFile()
+        {
+
+            try
+            {
+                BinaryReader reader = new BinaryReader(File.OpenRead(filename[0]));
+                Console.WriteLine(reader.ReadString());
+                Console.WriteLine(reader.ReadInt32());
+                reader.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Error. Press any key.");
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Error. Null Reference. Press any key.");
+            }
+        }
+    }
