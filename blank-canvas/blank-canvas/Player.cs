8cff0ac874d17fcdf6a45946e92e841d59f5a018
Seth Weidman (RIT Student) <scw4628@ad.rit.edu>
2016-03-07 17:21:59 -0500

    removed redundant methods, fixed enemy constructor

--- blank-canvas/blank-canvas/Player.cs
+++ blank-canvas/blank-canvas/Player.cs
@@ -17,20 +17,10 @@ namespace blank_canvas
         Rectangle playerRec;
 
         //constructor
-        public Player(Rectangle pRec)
+        public Player(Rectangle pRec):base(pRec)
         {
             playerRec = new Rectangle(pRec.X, pRec.Y, pRec.Width, pRec.Height);
         }
-
-        //methods from the Parent
-
-        //take damage(when colliding with an enemy/projectile, health gets lowered
-        public void takeDamage()
-        {
-            //only real change is how much damage the player takes depending on each enemy
-        }
-
-        //methods not from the Parent
 
         //change colors
         public void colorChange()
