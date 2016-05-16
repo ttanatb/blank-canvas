29d9d35ab470543acf4fe39863b90e9522092bad
Tanat Boozayaangool (RIT Student) <txb9038@ad.rit.edu>
2016-05-15 20:02:02 -0400

    Fixed stageReader issues with order of files, renamed files

--- blank-canvas/blank-canvas/Game1.cs
+++ blank-canvas/blank-canvas/Game1.cs
@@ -38,8 +38,6 @@ namespace blank_canvas
 
         // enum variables
         GameState state;
-        Level lvl;
-
         Camera camera;
 
         public Game1()
@@ -67,8 +65,6 @@ namespace blank_canvas
             //instantializes the initial GameState
             state = GameState.MainMenu;
             pointerNum = 0;
-
-            lvl = 0;
 
             base.Initialize();
         }
@@ -154,8 +150,17 @@ namespace blank_canvas
                     }
                     catch (NextLevelException)
                     {
-                        stageManager.NextLevel();
-                        stageManager.LoadContent(contentManager);
+                        if (stageManager.Level == 4)
+                        {
+                            state = GameState.MainMenu;
+                            pointerNum = 0;
+                            stageManager.Level = 0;
+                        }
+                        else
+                        {
+                            stageManager.NextLevel();
+                            stageManager.LoadContent(contentManager);
+                        }
                     }
 
                     //  updates the gameplay based on any input to change the state
@@ -204,7 +209,6 @@ namespace blank_canvas
                 switch(pointerNum)
                 {
                     case 0:
-                        stageManager.Level = 0;
                         stageManager.LoadContent(contentManager);
                         state = GameState.Gameplay;
                         break;
